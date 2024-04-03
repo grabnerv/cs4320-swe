43e81990755a9cc6187ed40ababd364f7f8873c6
DownrightNifty <51222679+DownrightNifty@users.noreply.github.com>
2024-04-03 17:23:15 -0500

    initial db connection

---
+++ src/Group2_iCLOTHINGApp/Controllers/AdministratorsController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Group2_iCLOTHINGApp.Models;
+
+namespace Group2_iCLOTHINGApp.Controllers
+{
+    public class AdministratorsController : Controller
+    {
+        private Group2_iCLOTHINGDBEntities db = new Group2_iCLOTHINGDBEntities();
+
+        // GET: Administrators
+        public ActionResult Index()
+        {
+            var administrator = db.Administrator.Include(a => a.UserAccessLevel);
+            return View(administrator.ToList());
+        }
+
+        // GET: Administrators/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Administrator administrator = db.Administrator.Find(id);
+            if (administrator == null)
+            {
+                return HttpNotFound();
+            }
+            return View(administrator);
+        }
+
+        // GET: Administrators/Create
+        public ActionResult Create()
+        {
+            ViewBag.userId = new SelectList(db.UserAccessLevel, "userID", "userAccess");
+            return View();
+        }
+
+        // POST: Administrators/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "userId,adminName,adminEmail,dateHired")] Administrator administrator)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Administrator.Add(administrator);
+
+                //try
+                //{
+
+                db.SaveChanges();
+
+                //}
+                //catch (DbEntityValidationException e)
+                //{
+                //    foreach (var eve in e.EntityValidationErrors)
+                //    {
+                //        Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                //            eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                //        foreach (var ve in eve.ValidationErrors)
+                //        {
+                //            Console.WriteLine("- Property: \"{0}\", Error: \"{1}\"",
+                //                ve.PropertyName, ve.ErrorMessage);
+                //        }
+                //    }
+                //    throw;
+                //}
+
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.userId = new SelectList(db.UserAccessLevel, "userID", "userAccess", administrator.userId);
+            return View(administrator);
+        }
+
+        // GET: Administrators/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Administrator administrator = db.Administrator.Find(id);
+            if (administrator == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.userId = new SelectList(db.UserAccessLevel, "userID", "userAccess", administrator.userId);
+            return View(administrator);
+        }
+
+        // POST: Administrators/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "userId,adminName,adminEmail,dateHired")] Administrator administrator)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(administrator).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.userId = new SelectList(db.UserAccessLevel, "userID", "userAccess", administrator.userId);
+            return View(administrator);
+        }
+
+        // GET: Administrators/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Administrator administrator = db.Administrator.Find(id);
+            if (administrator == null)
+            {
+                return HttpNotFound();
+            }
+            return View(administrator);
+        }
+
+        // POST: Administrators/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Administrator administrator = db.Administrator.Find(id);
+            db.Administrator.Remove(administrator);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
